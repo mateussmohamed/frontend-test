a933a6952241859199f797a30e6c4505b4d77454
Mateus Santana <mateuss.dev@gmail.com>
2020-05-28 17:49:12 -0300

    feat: add home module

---
+++ src/modules/home/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Heading } from "rebass/styled-components";
+import { useHistory } from "react-router-dom";
+import ProtectedLayout from "@b8/modules/ui/protected-layout";
+
+import { getUser, logout } from "@b8/services/auth";
+
+function Home() {
+  const user = getUser();
+  const history = useHistory();
+
+  function handleLogout() {
+    history.replace("/signin");
+    logout();
+  }
+
+  return (
+    <ProtectedLayout title={`Home - ${user.email}`}>
+      <Heading>Boas vendas!</Heading>
+
+      <Button onClick={handleLogout} mt={4} minWidth={120}>
+        Sair
+      </Button>
+    </ProtectedLayout>
+  );
+}
+
+export default Home;
